f7e064357f570c355cb9af53bb3ecbea587100cc
unknown <eder.cornejo.diaz@ucsp.edu.pe>
2020-05-27 19:22:04 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { QuienessomosComponent } from './components/quienessomos/quienessomos.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { OfertasComponent } from './components/ofertas/ofertas.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { EventosComponent } from './components/eventos/eventos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    QuienessomosComponent,
+    ProductosComponent,
+    OfertasComponent,
+    MenuComponent,
+    BlogComponent,
+    EventosComponent
   ],
   imports: [
     BrowserModule
